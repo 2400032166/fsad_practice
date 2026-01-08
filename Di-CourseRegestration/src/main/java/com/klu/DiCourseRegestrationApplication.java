38d9033ee0ded1f0ced5f33af6a59af831834316
2400032166 <2400032166@kluniversity.in>
2026-01-08 10:39:01 +0530

    Added Di-CourseRegestration project and gitignore

---
+++ Di-CourseRegestration/src/main/java/com/klu/DiCourseRegestrationApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiCourseRegestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiCourseRegestrationApplication.class, args);
+	}
+
+}
